e064e00595d1523b596d6904d79f43bacd55e69f
Lucas Maia <lucasluc4@hotmail.com>
2025-10-30 22:24:48 -0300

    Finished action in add_patrimony_modal

--- src/patrimony/modal/add_patrimony_modal.ts
+++ src/patrimony/modal/add_patrimony_modal.ts
@@ -1,5 +1,6 @@
-import { App, Modal, Setting, Notice } from 'obsidian';
+import {App, Modal, Setting, Notice, TFile} from 'obsidian';
 import { Asset } from "../../asset/asset";
+import {AssetType} from "../../asset/asset_type";
 
 export class AddPatrimonyModal extends Modal {
 	constructor(app: App) {
@@ -13,8 +14,8 @@ export class AddPatrimonyModal extends Modal {
 		let asset: Asset;
 		let yearInput: string = currentYear.toString();
 		let monthInput: string = currentMonth < 10 ? "0" + currentMonth : currentMonth.toString();
-		let period: string;
-		let value: number;
+		let period: string = yearInput + "-" + monthInput;
+		let patrimonyValue: number = 0;
 
 		new Setting(this.contentEl)
 			.setName('Year')
@@ -55,41 +56,97 @@ export class AddPatrimonyModal extends Modal {
 					})
 			);
 
-		// new Setting(this.contentEl)
-		// 	.addButton((btn) =>
-		// 		btn
-		// 			.setButtonText('Create')
-		// 			.setCta()
-		// 			.onClick(async () => {
-		// 				if (!name || !name.length) {
-		// 					new Notice('Name cannot be empty');
-		// 					return;
-		// 				}
-		//
-		// 				const fileContent = "---\n" +
-		// 					"Type: " + type + "\n" +
-		// 					"Active: true\n" +
-		// 					"---\n";
-		//
-		// 				const path = "finance/assets";
-		// 				const fileName = name + ".md";
-		// 				const filePath = path + "/" + fileName;
-		// 				try {
-		//
-		// 					const existingFolder = app.vault.getAbstractFileByPath(path);
-		// 					if (!existingFolder) {
-		// 						await this.app.vault.createFolder(path);
-		// 					}
-		//
-		// 					const file = await this.app.vault.create(filePath, fileContent);
-		// 					await this.app.workspace.getLeaf(false).openFile(file);
-		// 				} catch (e) {
-		// 					new Notice('An error occurred while creating new asset file. ' +
-		// 						'Check if file already exists.');
-		// 					return;
-		// 				}
-		//
-		// 				this.close();
-		// 			}));
+		const currentAssets: Asset[] = [];
+		const folder = this.app.vault.getFolderByPath("finance/assets");
+		folder?.children.forEach((child) => {
+			if (child instanceof TFile && child.extension === "md") {
+				const assetFile = child as TFile;
+				const frontmatter = this.app.metadataCache.getFileCache(assetFile)?.frontmatter;
+
+				if (frontmatter) {
+					try {
+						const active = frontmatter.Active as boolean;
+						const assetType = frontmatter.Type as AssetType;
+
+						currentAssets.push(new Asset(assetType, assetFile.basename, active));
+					} catch (e) {
+						console.error(e);
+					}
+				}
+			}
+		});
+
+		new Setting(this.contentEl)
+			.setName('Asset')
+			.addDropdown((dropdown) => {
+
+				currentAssets.forEach((currentAsset) => {
+					if (currentAsset.isActive()) {
+						dropdown.addOption(currentAsset.getName(), currentAsset.getName());
+						if (!asset) {
+							asset = currentAsset;
+							dropdown.setValue(currentAsset.getName());
+						}
+					}
+
+				});
+
+				dropdown
+					.onChange((value: string) => {
+						currentAssets.forEach((currentAsset) => {
+							if (value === currentAsset.getName()) {
+								asset = currentAsset;
+							}
+						})
+					});
+			});
+
+		new Setting(this.contentEl)
+			.setName('Value')
+			.addText((text) =>
+				text.setValue("0.00")
+					.onChange((value) => {
+						const numericInput = Number(value.replace(/\D/g, '')) / 100;
+						const input = numericInput.toFixed(2);
+						text.setValue(input);
+						patrimonyValue = numericInput;
+					})
+			);
+
+		new Setting(this.contentEl)
+			.addButton((btn) =>
+				btn
+					.setButtonText('Create')
+					.setCta()
+					.onClick(async () => {
+						if (!asset || !period || !patrimonyValue) {
+							new Notice('All fields are required');
+							return;
+						}
+
+						const fileContent = "---\n" +
+							"Value: " + patrimonyValue.toFixed(2) + "\n" +
+							"---\n";
+
+						const path = "finance/patrimony/" + period;
+						const fileName = asset.getName() + ".md";
+						const filePath = path + "/" + fileName;
+						try {
+
+							const existingFolder = app.vault.getAbstractFileByPath(path);
+							if (!existingFolder) {
+								await this.app.vault.createFolder(path);
+							}
+
+							const file = await this.app.vault.create(filePath, fileContent);
+							await this.app.workspace.getLeaf(false).openFile(file);
+						} catch (e) {
+							new Notice('An error occurred while creating new patrimony file. ' +
+								'Check if file ' + filePath + ' already exists.');
+							return;
+						}
+
+						this.close();
+					}));
 	}
 }
