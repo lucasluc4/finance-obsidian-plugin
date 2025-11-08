b438b3e6b97c47091fd120b835abddcde0ad0501
Lucas Maia <lucasluc4@hotmail.com>
2025-11-08 15:43:49 -0300

    Added toFixed on accounting details

--- src/accounting/file/accounting_file_setting.ts
+++ src/accounting/file/accounting_file_setting.ts
@@ -5,29 +5,29 @@ import {Accounting} from "../accounting";
 export class AccountingFileSetting implements FinanceFileSetting<Accounting> {
 	getFileContent(value: Accounting): string {
 		return "---\n" +
-			"Real Estate Patrimony: " + value.getRealEstatePatrimony() + "\n" +
-			"Financial Patrimony: " + value.getFinancialPatrimony() + "\n" +
-			"Investment Financial Patrimony: " + value.getInvestmentFinancialPatrimony() + "\n" +
-			"Deposit Financial Patrimony: " + value.getDepositFinancialPatrimony() + "\n" +
-			"Total Patrimony: " + value.getTotalPatrimony() + "\n" +
-			"Total Net Patrimony: " + value.getTotalNetPatrimony() + "\n" +
-			"Total Income: " + value.getTotalIncome() + "\n" +
-			"Total Investment Deposit: " + value.getTotalInvestmentDeposit() + "\n" +
-			"Total Reserve: " + value.getTotalReserve() + "\n" +
-			"Reserve Diff: " + value.getReserveDiff() + "\n" +
+			"Real Estate Patrimony: " + value.getRealEstatePatrimony().toFixed(2) + "\n" +
+			"Financial Patrimony: " + value.getFinancialPatrimony().toFixed(2) + "\n" +
+			"Investment Financial Patrimony: " + value.getInvestmentFinancialPatrimony().toFixed(2) + "\n" +
+			"Deposit Financial Patrimony: " + value.getDepositFinancialPatrimony().toFixed(2) + "\n" +
+			"Total Patrimony: " + value.getTotalPatrimony().toFixed(2) + "\n" +
+			"Total Net Patrimony: " + value.getTotalNetPatrimony().toFixed(2) + "\n" +
+			"Total Income: " + value.getTotalIncome().toFixed(2) + "\n" +
+			"Total Investment Deposit: " + value.getTotalInvestmentDeposit().toFixed(2) + "\n" +
+			"Total Reserve: " + value.getTotalReserve().toFixed(2) + "\n" +
+			"Reserve Diff: " + value.getReserveDiff().toFixed(2) + "\n" +
 			"Reserve Balance: \"" +
 				JSON.stringify(value.getReserveBalance()).replace(/"/g, "'") + "\"\n" +
-			"Patrimony Diff: " + value.getPatrimonyDiff() + "\n" +
-			"Financial Patrimony Diff: " + value.getFinancialPatrimonyDiff() + "\n" +
-			"Investment Patrimony Diff: " + value.getInvestmentPatrimonyDiff() + "\n" +
-			"Deposit Patrimony Diff: " + value.getDepositPatrimonyDiff() + "\n" +
-			"Real Estate Patrimony Diff: " + value.getRealEstatePatrimonyDiff() + "\n" +
-			"Investment Interest: " + value.getInvestmentInterest() + "\n" +
-			"Percentage Investment Interest: " + value.getPercentageInvestmentInterest() + "\n" +
-			"Net Economy: " + value.getNetEconomy() + "\n" +
-			"Percentage Net Economy: " + value.getPercentageNetEconomy() + "\n" +
-			"Financial Net Economy: " + value.getFinancialNetEconomy() + "\n" +
-			"Percentage Financial Net Economy: " + value.getPercentageFinancialNetEconomy() + "\n" +
+			"Patrimony Diff: " + value.getPatrimonyDiff().toFixed(2) + "\n" +
+			"Financial Patrimony Diff: " + value.getFinancialPatrimonyDiff().toFixed(2) + "\n" +
+			"Investment Patrimony Diff: " + value.getInvestmentPatrimonyDiff().toFixed(2) + "\n" +
+			"Deposit Patrimony Diff: " + value.getDepositPatrimonyDiff().toFixed(2) + "\n" +
+			"Real Estate Patrimony Diff: " + value.getRealEstatePatrimonyDiff().toFixed(2) + "\n" +
+			"Investment Interest: " + value.getInvestmentInterest().toFixed(2) + "\n" +
+			"Percentage Investment Interest: " + value.getPercentageInvestmentInterest().toFixed(6) + "\n" +
+			"Net Economy: " + value.getNetEconomy().toFixed(2) + "\n" +
+			"Percentage Net Economy: " + value.getPercentageNetEconomy().toFixed(6) + "\n" +
+			"Financial Net Economy: " + value.getFinancialNetEconomy().toFixed(2) + "\n" +
+			"Percentage Financial Net Economy: " + value.getPercentageFinancialNetEconomy().toFixed(6) + "\n" +
 			"---\n";
 	}
 
